c7c6e771a2be673deab90275b97be1f9d2d9767d
kienkaka150499 <75771331+kienkaka150499@users.noreply.github.com>
2022-07-03 10:49:45 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -31,86 +31,62 @@ class _MyAppState extends State<MyAppState> {
           title: const Text('Calculator App'),
         ),
         body: SafeArea(
-          child: Center(
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    child: Image.asset(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
                       'assets/images/calculator.png',
                       width: 150,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    child: SizedBox(
-                      width: 200,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            gapPadding: 10,
-                          ),
-                          labelText: 'Nhập số A',
-                        ),
-                        controller: controllerA,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                    child: SizedBox(
-                      width: 200,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            gapPadding: 10,
-                          ),
-                          labelText: 'Nhập số B',
-                        ),
-                        controller: controllerB,
-                      ),
-                    ),
-                  ),
-                  Text('Kết quả: $result'),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                          child: SizedBox(
-                            width: 100,
-                            child: ElevatedButton(
-                              onPressed: () {
-                                setState(() {
-                                  var numA = double.tryParse(controllerA.text);
-                                  var numB = double.tryParse(controllerB.text);
-                                  if (numA == null || numB == null) {
-                                    result =
-                                        'Bạn chưa nhập giá trị hoặc nhập không đúng!';
-                                  } else {
-                                    result = '${numA + numB}';
-                                  }
-                                });
-                              },
-                              child: const Text(
-                                '+',
-                                style: TextStyle(fontSize: 30),
-                              ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      child: SizedBox(
+                        width: 200,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              gapPadding: 10,
                             ),
+                            labelText: 'Nhập số A',
                           ),
+                          controller: controllerA,
                         ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                          child: SizedBox(
-                            width: 100,
-                            child: ElevatedButton(
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                      child: SizedBox(
+                        width: 200,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              gapPadding: 10,
+                            ),
+                            labelText: 'Nhập số B',
+                          ),
+                          controller: controllerB,
+                        ),
+                      ),
+                    ),
+                    Text('Kết quả: $result'),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                            child: SizedBox(
+                              width: 60,
+                              child: ElevatedButton(
                                 onPressed: () {
                                   setState(() {
                                     var numA = double.tryParse(controllerA.text);
@@ -119,80 +95,109 @@ class _MyAppState extends State<MyAppState> {
                                       result =
                                           'Bạn chưa nhập giá trị hoặc nhập không đúng!';
                                     } else {
-                                      result = '${numA - numB}';
+                                      result = '${numA + numB}';
                                     }
                                   });
                                 },
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.red,
-                                ),
                                 child: const Text(
-                                  '-',
+                                  '+',
                                   style: TextStyle(fontSize: 30),
-                                )),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                          child: SizedBox(
-                            width: 100,
-                            child: ElevatedButton(
-                              onPressed: () {
-                                setState(() {
-                                  var numA = double.tryParse(controllerA.text);
-                                  var numB = double.tryParse(controllerB.text);
-                                  if (numA == null || numB == null) {
-                                    result =
-                                        'Bạn chưa nhập giá trị hoặc nhập không đúng!';
-                                  } else {
-                                    result = '${numA * numB}';
-                                  }
-                                });
-                              },
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.greenAccent,
-                              ),
-                              child: const Text(
-                                'X',
-                                style: TextStyle(fontSize: 30),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                          child: SizedBox(
-                            width: 100,
-                            child: ElevatedButton(
-                                onPressed: () {
-                                  setState(() {
-                                    var numA = double.tryParse(controllerA.text);
-                                    var numB = double.tryParse(controllerB.text);
-                                    if (numA == null || numB == null) {
-                                      result =
-                                          'Bạn chưa nhập giá trị hoặc nhập không đúng!';
-                                    } else {
-                                      if (numB == 0) {
-                                        result = 'Không thể chia cho số 0';
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                            child: SizedBox(
+                              width: 60,
+                              child: ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      var numA =
+                                          double.tryParse(controllerA.text);
+                                      var numB =
+                                          double.tryParse(controllerB.text);
+                                      if (numA == null || numB == null) {
+                                        result =
+                                            'Bạn chưa nhập giá trị hoặc nhập không đúng!';
                                       } else {
-                                        result = '${numA / numB}';
+                                        result = '${numA - numB}';
                                       }
+                                    });
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.red,
+                                  ),
+                                  child: const Text(
+                                    '-',
+                                    style: TextStyle(fontSize: 30),
+                                  )),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                            child: SizedBox(
+                              width: 60,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    var numA = double.tryParse(controllerA.text);
+                                    var numB = double.tryParse(controllerB.text);
+                                    if (numA == null || numB == null) {
+                                      result =
+                                          'Bạn chưa nhập giá trị hoặc nhập không đúng!';
+                                    } else {
+                                      result = '${numA * numB}';
                                     }
                                   });
                                 },
                                 style: ElevatedButton.styleFrom(
-                                  primary: Colors.deepOrangeAccent,
+                                  primary: Colors.greenAccent,
                                 ),
                                 child: const Text(
-                                  '/',
+                                  'X',
                                   style: TextStyle(fontSize: 30),
-                                )),
+                                ),
+                              ),
+                            ),
                           ),
-                        )
-                      ],
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                            child: SizedBox(
+                              width: 60,
+                              child: ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      var numA =
+                                          double.tryParse(controllerA.text);
+                                      var numB =
+                                          double.tryParse(controllerB.text);
+                                      if (numA == null || numB == null) {
+                                        result =
+                                            'Bạn chưa nhập giá trị hoặc nhập không đúng!';
+                                      } else {
+                                        if (numB == 0) {
+                                          result = 'Không thể chia cho số 0';
+                                        } else {
+                                          result = '${numA / numB}';
+                                        }
+                                      }
+                                    });
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.deepOrangeAccent,
+                                  ),
+                                  child: const Text(
+                                    '/',
+                                    style: TextStyle(fontSize: 30),
+                                  )),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
